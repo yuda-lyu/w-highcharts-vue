eb50d5816365a199df5e48d9631ce5a40b762adb
yuda-lyu <firsemisphere@gmail.com>
2026-08-19 00:31:17 +0800

    modify: update pks

--- toolg/gDistRollupComps.mjs
+++ toolg/gDistRollupComps.mjs
@@ -13,7 +13,7 @@ rollupFiles({
         return 'w-highcharts-vue' //因來源檔為WHighchartsVueAll.vue, 故強制指定輸出名稱為套件名稱
     },
     globals: {
-        'vue': 'Vue', //統一使用window內Vue, 故此處不打包進來, 若vue-cli則自動將Vue關聯至window內的Vue
+        'vue': 'Vue',
     },
     external: [
         'vue',
